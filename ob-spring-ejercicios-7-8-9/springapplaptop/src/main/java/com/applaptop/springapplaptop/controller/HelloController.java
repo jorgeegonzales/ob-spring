e31d6eea1ecad2748e53ed49da055ca013c708ca
jorgeegonzales <jorgeegonzales10@gmail.com>
2022-09-23 23:55:12 -0500

    1 commit para los ejercicios-7-8-9 del curso de spring de open bootcamp, avance hasta caso de test del metodo create

---
+++ ob-spring-ejercicios-7-8-9/springapplaptop/src/main/java/com/applaptop/springapplaptop/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.applaptop.springapplaptop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello my friend!";
+    }
+}
---
+++ ob-spring-ejercicios-7-8-9/springapplaptop/src/test/java/com/applaptop/springapplaptop/controller/LaptopControllerTest.java
@@ -0,0 +1,85 @@
+package com.applaptop.springapplaptop.controller;
+
+import com.applaptop.springapplaptop.entities.Laptop;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class LaptopControllerTest {
+
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp(){
+        restTemplateBuilder = restTemplateBuilder.rootUri("http://localhost:" + port);
+        testRestTemplate = new TestRestTemplate(restTemplateBuilder);
+    }
+
+    @Test
+    void hello() {
+        ResponseEntity<String>  response = testRestTemplate.getForEntity("/hello",String.class);
+
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals("Hello my friend!",response.getBody());
+
+    }
+
+    @Test
+    void findAll() {
+        ResponseEntity<Laptop[]> response = testRestTemplate.getForEntity("/api/laptops", Laptop[].class);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+
+        List<Laptop> laptops = Arrays.asList(response.getBody());
+        System.out.println(laptops.size());
+    }
+
+    @Test
+    void findOneById() {
+       ResponseEntity<Laptop> response = testRestTemplate.getForEntity("/api/laptops/1", Laptop.class);
+       assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    void create() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        String json = """
+                {
+                     "brand": "Lenovo creado desde Spring Test",
+                     "model": "Legión 5",
+                     "processor": "AMD RYZEN R5",
+                     "hardDrive": 512,
+                     "ram": 8,
+                     "screenSize": 15.6,
+                     "operatingSystem": "Windows 11 Home - Español",
+                     "madeIn": "China"
+                 }
+                """;
+
+        HttpEntity<String> request = new HttpEntity<>(json,headers);
+        ResponseEntity<Laptop> response = testRestTemplate.exchange("/api/laptops",HttpMethod.POST,request,Laptop.class);
+        Laptop result = response.getBody();
+
+        assertEquals(1L,result.getId());
+        assertEquals("Lenovo creado desde Spring Test",result.getBrand());
+    }
+}
